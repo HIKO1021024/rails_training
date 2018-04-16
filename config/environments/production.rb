6c8904f0a9fefb4f139eb9da4e6e9839a3823239
hiko <ana@m-hiko.info>
2018-04-16 07:21:12 +0000

    デモユーザーを追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # Action Cable endpoint configuration
+# config.action_cable.url = 'wss://example.com/cable'
+ config.action_cable.allowed_request_origins = [ /http:\/\/.*/ ]
+# Don't mount Action Cable in the main server process.
+config.action_cable.mount_path = nil
 end
